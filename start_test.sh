5e22770fc3f24faec213cc12050c5473f0edf04c
wuxiuheng <wxhbuaa@gmail.com>
2018-04-11 14:06:28 +0800

    add usage and param check for start_test.sh

--- start_test.sh
+++ start_test.sh
@@ -1,14 +1,55 @@
 #! /bin/bash
-export AFL_NO_AFFINITY=1
+
+usage ()
+{
+    echo 'Usage: ./start_test.sh <afl-binary> <target-binary> <start of n in out-n/> <number of tests> <session name of tmux>'
+    echo 'Example: ./start_test.sh `which afl-fuzz` ./pngfix 6 6 pngfix-afl-2.52b'
+    exit 1
+}
+
+if [[ ! -z $1 ]]
+then
+    [[ `command -v $1` ]] || { echo "$1 command not found! Aborting."; usage;}
+fi
+
+if [[ ! -z $2 ]]
+then
+    [[ -f $2 ]] || { echo "Target binary not found! Aborting.";usage;}
+fi
+
+if [[ ! -z $3 ]]
+then
+    if [[ -d "out-$3" ]]
+    then
+        echo "./out-$3/ exists, danger! Aborting.";
+        usage;
+    fi
+    if [[ ! $3 =~ ^[0-9]+$ ]]
+    then
+        echo "$3 is not a positive number. Aborting."
+        usage;
+    fi
+fi
+
+if [[ ! -z $4 ]] && ([[ ! $4 =~ ^[0-9]+$ ]] || (( A <= 0)) )
+then
+    echo "$4 is not a positive number. Aborting"
+    usage;
+fi
+
+
+if [[ "$#" -ne 5 ]]
+then usage
+fi
+
 
 AFL_Binary=$1
 Target=$2
-
 Start_of_Tests=$3
 Num_of_Tests=$4
-
 Tmux_Session=$5
-tmux has-session -t $Tmux_Session 2> /dev/null
+
+tmux has-session -t $Tmux_Session 2> /dev/null;
 
 if [[ $? != 0 ]]; then
     tmux new-session -d -s $Tmux_Session;
@@ -18,5 +59,7 @@ if [[ $? != 0 ]]; then
         tmux send-keys  -t "$Tmux_Session:$i" "export AFL_NO_AFFINITY=1"   C-m ;
         tmux send-keys  -t "$Tmux_Session:$i" "$AFL_Binary -i in/ -o out-$output_dir_num/ -t 50 -d -p fast -- $Target @@"   C-m ;
     done;
+else
+    printf "tmux session $Tmux_Session exists.\n=========\n`tmux ls 2>/dev/null`."
 fi;
 
