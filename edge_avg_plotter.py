74621493fe8bb2b59929b627d418013c60f03128
Your Name <you@example.com>
2018-04-21 21:03:08 +0800

    basic update to edge_plotter

--- edge_avg_plotter.py
+++ edge_avg_plotter.py
@@ -5,19 +5,25 @@ import shutil
 import sys
 import matplotlib.pyplot as plt
 
+from collections import OrderedDict
 from common_utils import *
 from args import *
 
 
 def sanitize_config(config):
-    required_params = ['plot_file', 'targets', 'bucket']
+    required_params = ['targets', 'bucket', 'output_dir', 'project']
 
     valid_buckets = ['second', 'minute', 'hour', 'sec', 'min', 'hour', 's', 'm', 'h']
+
+    missing = False
 
     for param in required_params:
         if param not in config:
             danger("%s is missing in the config file" % param)
-            return False
+            missing = True
+
+    if missing:
+        return False
 
     if config['bucket'].lower() not in valid_buckets:
         danger("Invalid bucket unit")
@@ -27,6 +33,9 @@ def sanitize_config(config):
     if len(config['targets']) == 0:
         danger("No target specified")
         return False
+
+    if not config['output_dir'].endswith('/'):
+        config['output_dir'] += '/'
 
     if 'max_bin' not in config:
         config['max_bin'] = -1
@@ -66,10 +75,18 @@ def main():
         if not sanitize_config(config):
             sys.exit(1)
 
+        if os.path.isdir(config['output_dir']):
+            warn("output dir %s exists, we will clear it this time" % config['output_dir'])
+            shutil.rmtree(config['output_dir'])
+
+        os.makedirs(config['output_dir'])
+
         targets = config['targets']
 
         # key: target name; value: average edge no
-        target_edge_dict = {}
+        target_avg_edge_dict = {}
+        # key: target name; value: {id: average edge no}
+        target_detail_edge_dict = {}
 
         for target_key in targets:
             info("checking for %s" % target_key)
@@ -84,7 +101,10 @@ def main():
             first_run = True
             max_bin = config['max_bin']
 
-            for data_file in data_files:
+            target_detail_edge_dict[target_key] = OrderedDict()
+
+            for (did, data_file) in enumerate(data_files):
+                target_detail_edge_dict[target_key][did] = []
                 with open(data_file) as data_fd:
                     lines = data_fd.readlines()
                     for (i, line) in enumerate(lines):
@@ -94,8 +114,9 @@ def main():
                         if bin_no != i+1:
                             danger("invalid file - bin_no(%d), line_no(%d)" % (bin_no, i), 1)
                             sys.exit(1)
+                        target_detail_edge_dict[target_key][did].append(edge_no)
                         if bin_no == len(lines):
-                            ok("total: %d" % edge_no, 1)
+                            ok("%s: %d" % (data_file, edge_no), 1)
                         if first_run:
                             accumulates.append(edge_no)
                         else:
@@ -104,19 +125,22 @@ def main():
 
             avgs = list(map(lambda acc: float(acc)/float(base_no), accumulates))
             ok("total avg: %.2f" % avgs[-1], 1)
-            target_edge_dict[target_key] = avgs
+            target_avg_edge_dict[target_key] = avgs
 
         # then we need to process the data and draw the plot
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
+        avg_fig_id = 1
+        detail_fig_id = 2
+        avg_fig = plt.figure(avg_fig_id)
+        avg_ax = avg_fig.add_subplot(111)
 
         # sort the group names, make sure every time the order is consistent
-        group_names = list(target_edge_dict.keys())
+        group_names = list(target_avg_edge_dict.keys())
         group_names.sort()
+        bucket = config['bucket']
 
         for group_name in group_names:
 
-            avgs = target_edge_dict[group_name]
+            avgs = target_avg_edge_dict[group_name]
             # max_bin = max(known_bins)
             max_bin = len(avgs)
 
@@ -127,16 +151,35 @@ def main():
                 x_vals.append(bin_no)
                 y_vals.append(avgs[bin_no])
 
-            ax.plot(x_vals[1:], y_vals[1:], label=group_name)
+            avg_ax.plot(x_vals[1:], y_vals[1:], label=group_name)
 
-        bucket = config['bucket']
-        edge_no_time_plot_filename = config['plot_file']
-        ax.set(xlabel='time (%s)' % bucket, ylabel='avg edge no #',
-               title='No of edges covered over time')
-        ax.grid()
-        ax.legend()
+            detail_fig = plt.figure(detail_fig_id)
+            detail_ax = detail_fig.add_subplot(111)
 
-        fig.savefig(edge_no_time_plot_filename)
+            target_detail = target_detail_edge_dict[group_name]
+            for did in target_detail:
+                details = target_detail[did]
+                max_bin = len(details)
+                x_vals = []
+                y_vals = []
+                for bin_no in range(0, max_bin):
+                    x_vals.append(bin_no)
+                    y_vals.append(details[bin_no])
+                detail_ax.plot(x_vals[1:], y_vals[1:], label=group_name+'-'+str(did))
+
+            detail_plot_filename = config['output_dir'] + config['project'] + '-' + group_name + '-edge-time.pdf'
+            detail_ax.set(xlabel='time (%s)' % bucket, ylabel='edge no #')
+            detail_ax.legend()
+            detail_fig.savefig(detail_plot_filename)
+
+            detail_fig_id += 1
+
+        avg_plot_filename = config['output_dir'] + config['project'] + '-edge-time.pdf'
+        avg_ax.set(xlabel='time (%s)' % bucket, ylabel='avg edge no #')
+        # avg_ax.grid()
+        avg_ax.legend()
+
+        avg_fig.savefig(avg_plot_filename)
         # plt.show()
 
 
